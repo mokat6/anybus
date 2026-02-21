c124708031531471dbf3da185b6e8ebb171ba9fe
Momo <github2@5208866.xyz>
2026-02-21 17:47:33 +0200

    swapped MySQL > Postgres

--- back-end/build.gradle.kts
+++ back-end/build.gradle.kts
@@ -19,10 +19,10 @@
 
 	dependencies {
 		implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+		implementation("org.postgresql:postgresql:42.6.0") 
 		implementation("org.springframework.boot:spring-boot-starter-security")
 		implementation("org.springframework.boot:spring-boot-starter-web")
 		developmentOnly("org.springframework.boot:spring-boot-devtools")
-		runtimeOnly("com.mysql:mysql-connector-j")
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
 		testImplementation("org.springframework.security:spring-security-test")
 		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
