ade5e554e9e512c761d5817925a0ac2125d2d679
Momo <github2@5208866.xyz>
2026-02-21 16:45:16 +0200

    added docker, disabled security

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# ---------- FRONTEND BUILD ----------
+    FROM node:20-alpine AS frontend-build
+    WORKDIR /app/frontend
+    COPY front-end/package*.json ./
+    RUN npm install
+    COPY front-end/ .
+    RUN npm run build
+    
+    
+    # ---------- BACKEND BUILD ----------
+    FROM gradle:8.5-jdk21-alpine AS backend-build
+    WORKDIR /app
+    COPY back-end/ ./back-end/
+    
+    # Copy frontend build into Spring static folder
+    COPY --from=frontend-build /app/frontend/build \
+        /app/back-end/src/main/resources/static
+    
+    WORKDIR /app/back-end
+    RUN gradle bootJar --no-daemon
+    
+    
+    # ---------- RUNTIME ----------
+    FROM eclipse-temurin:21-jre-alpine
+    WORKDIR /app
+    
+    COPY --from=backend-build /app/back-end/build/libs/*.jar app.jar
+    
+    EXPOSE 8080
+    
+    ENTRYPOINT ["java","-jar","app.jar"]
--- back-end/src/main/java/info/anyksciaibus/security/WebSecurityConfig.java
+++ back-end/src/main/java/info/anyksciaibus/security/WebSecurityConfig.java
@@ -64,17 +64,19 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth ->
 //!!!!rules are evaluated in the order they are declared, and the first matching rule is applied
                         auth
-                                .requestMatchers("/holidays/al?").permitAll()
-                                .requestMatchers("/scheduleItem/singleTrip/*").permitAll()
-                                .requestMatchers("/scheduleItem/home/*").permitAll()
-
-                                .requestMatchers("/line/test/**").permitAll()
-                                .requestMatchers("/api/auth/**").permitAll()
-
-                                .requestMatchers("/swagger-ui/**").permitAll()
-
-                        // secured - only admin access
-                                .anyRequest().hasRole("ADMIN")
+                                                                .anyRequest().permitAll()
+//
+//                                .requestMatchers("/holidays/al?").permitAll()
+//                                .requestMatchers("/scheduleItem/singleTrip/*").permitAll()
+//                                .requestMatchers("/scheduleItem/home/*").permitAll()
+//
+//                                .requestMatchers("/line/test/**").permitAll()
+//                                .requestMatchers("/api/auth/**").permitAll()
+//
+//                                .requestMatchers("/swagger-ui/**").permitAll()
+//
+//                        // secured - only admin access
+//                                .anyRequest().hasRole("ADMIN")
 
 //                      unsecured - open for all:
 //                                .anyRequest().permitAll()
